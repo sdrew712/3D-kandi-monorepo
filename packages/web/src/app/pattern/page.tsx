383f5be029a01d34bd0f25a5b2e2fd1c3cf623d8
Sarah Drew <drew.sarah.e@gmail.com>
2024-08-07 22:52:39 -0700

    GRAPHQL!!!!

--- packages/web/src/app/pattern/page.tsx
+++ packages/web/src/app/pattern/page.tsx
@@ -3,10 +3,17 @@
 import styles from "../page.module.css";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { Excision } from "../../sample-patterns/excision";
 import { renderPattern } from "@/utils/renderPattern";
+import { useSuspenseQuery, gql } from "@apollo/client";
 
 export default function Pattern() {
+  const result = useSuspenseQuery(GET_PATTERN, {
+    variables: {
+      //todo: get pattern with ID
+      patternId: "123",
+    },
+  });
+
   return (
     <div className={styles.canvasContainer}>
       <Canvas
@@ -17,8 +24,26 @@ export default function Pattern() {
         <OrbitControls />
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        {renderPattern(Excision)}
+        {/* todo: fix this type */}
+        {renderPattern(result.data.pattern)}
       </Canvas>
     </div>
   );
 }
+
+const GET_PATTERN = gql`
+  query Query($patternId: String!) {
+    pattern(id: $patternId) {
+      id
+      userId
+      planes {
+        beads {
+          x
+          y
+          z
+          color
+        }
+      }
+    }
+  }
+`;
