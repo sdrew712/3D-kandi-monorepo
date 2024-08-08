fb9fd7d35fb49ab8c7389759cd164ba2fc61b340
Sarah Drew <drew.sarah.e@gmail.com>
2024-08-07 20:55:30 -0700

    Add getPattern core function

--- packages/core/graphql/pattern.resolver.ts
+++ packages/core/graphql/pattern.resolver.ts
@@ -11,19 +11,12 @@ import {
 } from "type-graphql";
 import "reflect-metadata";
 import { Pattern } from "./pattern.type";
+import { getPattern } from "../src/getPattern";
 
 @Resolver(Pattern)
 export class PatternResolver {
   @Query((_returns) => Pattern)
   async pattern(@Arg("id") id: string): Promise<Pattern> {
-    return {
-      id: "123",
-      userId: "123",
-      planes: [
-        {
-          beads: [{ x: 1, y: 2, z: 3, color: "#ffff" }],
-        },
-      ],
-    };
+    return getPattern({ id });
   }
 }
