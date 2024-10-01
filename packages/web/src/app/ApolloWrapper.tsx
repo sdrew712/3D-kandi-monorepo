9524779d1ae1fbe800098bef7cc54e0103ed7891
Sarah Drew <drew.sarah.e@gmail.com>
2024-09-30 21:29:45 -0700

    Use production api for prod

--- packages/web/src/app/ApolloWrapper.tsx
+++ packages/web/src/app/ApolloWrapper.tsx
@@ -8,8 +8,15 @@ import {
 } from "@apollo/experimental-nextjs-app-support";
 
 function makeClient() {
+  const env = process.env.NODE_ENV;
+
+  let uri = "https://ykpfkfot1d.execute-api.us-west-2.amazonaws.com";
+  if (env === "production") {
+    uri = "https://5kkazk3271.execute-api.us-west-2.amazonaws.com";
+  }
+
   const httpLink = new HttpLink({
-    uri: "https://ykpfkfot1d.execute-api.us-west-2.amazonaws.com/graphql",
+    uri: uri + "/graphql",
     fetchOptions: { cache: "no-store" },
   });
 
