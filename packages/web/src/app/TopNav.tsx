056ed67752bf0d5aaecb744f06c38891af62bd6d
Sarah Drew <drew.sarah.e@gmail.com>
2024-10-29 21:23:57 -0700

    add authOptions to topNav

--- packages/web/src/app/TopNav.tsx
+++ packages/web/src/app/TopNav.tsx
@@ -1,18 +1,35 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./page.module.css";
+import signOut from "../../../core/src/firebase/auth/signout";
+import { useAuth } from "./useAuth";
 
 export default function TopNav() {
   return (
     <nav className={styles.topNav}>
-      <div>
+      <div className={styles.centered}>
         <Link href="/">Home</Link>
 
         <Link href="/pattern">Pattern</Link>
       </div>
 
-      <div>
-        <Link href="/signin">Sign Out</Link>
-      </div>
+      {renderAuthOptions()}
     </nav>
+  );
+}
+
+function renderAuthOptions() {
+  const { user } = useAuth();
+
+  if (user) {
+    return <button onClick={signOut}>Sign out</button>;
+  }
+
+  return (
+    <div className={styles.centered}>
+      <Link href="/signin">Sign in</Link>
+      <Link href="/signup">Sign up</Link>
+    </div>
   );
 }
