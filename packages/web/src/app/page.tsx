ab6702400c80f6a80b15b2622c6b278f9997ed92
Sarah Drew <drew.sarah.e@gmail.com>
2024-12-09 20:32:18 -0700

    Add light home page styles

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useEffect } from "react";
+import styles from "../page.module.css";
 import WebGL from "three/examples/jsm/capabilities/WebGL.js";
+import Link from "next/link";
 
 export default function Home() {
   useEffect(() => {
@@ -17,5 +19,13 @@ export default function Home() {
 }
 
 function HomeContent() {
-  return <div>Welcome to my kandi site!</div>;
+  return (
+    <div id={styles.homePage}>
+      <h1>Welcome to my kandi site!</h1>
+      <h2>
+        To get started,&nbsp;
+        <Link href="/new">create a pattern.</Link>
+      </h2>
+    </div>
+  );
 }
