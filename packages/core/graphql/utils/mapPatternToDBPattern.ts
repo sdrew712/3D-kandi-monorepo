09f54d73ca9f206513a790622137e994b0cc6309
Sarah Drew <drew.sarah.e@gmail.com>
2025-01-22 22:42:24 -0700

    fix: not mapping sk correctly

--- packages/core/graphql/utils/mapPatternToDBPattern.ts
+++ packages/core/graphql/utils/mapPatternToDBPattern.ts
@@ -4,7 +4,7 @@ import { DBPattern } from "../../src/dbTypes";
 export function mapPatternToDBPattern(pattern: Pattern): DBPattern {
   return {
     pk: `USER#${pattern.userId}`,
-    sk: `PATTERN${pattern.id}`,
+    sk: `PATTERN#${pattern.id}`,
     planes: JSON.stringify(pattern.planes),
     title: pattern.title,
   };
